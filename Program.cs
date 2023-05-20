7723570de9fb6023b35f546ee5a4fb8077bce4f5
BhagyashriSabale <balisabale16@gmail.com>
2023-05-20 21:34:14 +0530

    [Bhagyashri] Add Valid First name

--- Program.cs
+++ Program.cs
@@ -5,6 +5,39 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Exception Problem");
+            try
+            {
+                
+               
+                string firstName = GetValidFirstName();
+                Console.WriteLine("Registration successful!");
+                Console.WriteLine("First Name: " + firstName);
+            }
+            catch (UserRegisterException ex)
+            {
+                Console.WriteLine("Registration failed: " + ex.Message);
+            }
+            
+        }
+        static string GetValidFirstName()
+        {
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new UserRegisterException("First Name cannot be empty.");
+            }
+            else if (firstName.Length < 3)
+            {
+                throw new UserRegisterException("First Name should have a minimum of 3 characters.");
+            }
+            else if (!char.IsUpper(firstName[0]))
+            {
+                throw new UserRegisterException("First Name should start with a capital letter.");
+            }
+
+            return firstName;
         }
     }
 }
---
+++ UserRegisterException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionUserRegistration
+{
+    internal class UserRegisterException: Exception
+    {
+        public UserRegisterException(string message):base(message) 
+        { }
+    }
+}
